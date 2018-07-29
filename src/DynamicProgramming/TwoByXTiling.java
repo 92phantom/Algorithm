99bed0969bb78afa12a543952fd5bb512f6f01c4
hyunjinPark <phj3372@hanyang.ac.kr>
2018-07-29 17:12:55 +0900

    2xN Tiling

---
+++ src/DynamicProgramming/TwoByXTiling.java
@@ -0,0 +1,13 @@
+package DynamicProgramming;
+
+public class TwoByXTiling {
+
+	
+	public static void main(String...args){
+		log("helloworld");
+	}
+	
+	public static void log(String text){
+		System.out.println(text);
+	}
+}
