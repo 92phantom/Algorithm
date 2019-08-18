b3b0b6d4601232b2b32c9530971687ffddde92fa
HyunJin Park <40868626+hyyyyjin@users.noreply.github.com>
2019-08-18 10:35:42 +0900

    moc5
    Study
    boj_2468    안전영역
    boj_17406  배열돌리기

---
+++ src/moc6/boj_17406.java
@@ -0,0 +1,29 @@
+package moc6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_17406 {
+
+	static int N;
+	static int[][] map;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+
+		map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+
+}
---
+++ src/moc6/boj_2468.java
@@ -0,0 +1,10 @@
+package moc6;
+
+public class boj_2468 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
